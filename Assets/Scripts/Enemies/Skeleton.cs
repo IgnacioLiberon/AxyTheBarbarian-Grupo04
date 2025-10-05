f4f68e40302982c78c804a7b917b341b8144e6d2
Luciano Fagioli <lbfagioli@miuandes.cl>
2025-10-05 19:50:16 -0300

    create skeleton, arrow and attack

--- Assets/Scripts/Enemies/Skeleton.cs
+++ Assets/Scripts/Enemies/Skeleton.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class Skeleton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject arrowPrefab; 
+    
+    // Set the point on the skeleton where the arrow appears
+    public Transform launchPoint; 
+    
+    public float throwCooldown = 2f; 
+    private float nextThrowTime;
 
-    // Update is called once per frame
     void Update()
     {
+        if (Time.time >= nextThrowTime)
+        {
+            ThrowArrow();
+            nextThrowTime = Time.time + throwCooldown;
+        }
+    }
+
+    void ThrowArrow()
+    {
+        // 1. Create the arrow at the launch point
+        GameObject arrowObject = Instantiate(arrowPrefab, launchPoint.position, launchPoint.rotation);
         
+        // 2. Get the Arrow script
+        Arrow arrowScript = arrowObject.GetComponent<Arrow>();
+        
+        // 3. Tell the arrow what direction to fly
+        if (arrowScript != null)
+        {
+            Vector2 throwDirection = new Vector2(Random.Range(0f, 1f), Random.Range(-1f, 0f)); 
+            arrowScript.SetDirection(throwDirection);
+        }
     }
 }
