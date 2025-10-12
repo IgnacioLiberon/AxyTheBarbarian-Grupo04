403635da5622c3fddea0fef0e256ac97401a9005
Alan Fischer <amfischer@miuandes.cl>
2025-10-12 19:52:12 -0300

    Walls

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEditor;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -6,9 +7,15 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     private Vector2 inputDirection = Vector2.zero;
 
+    private Rigidbody2D rb;
+    private AudioSource audioSource;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
 
     }
 
@@ -41,5 +48,14 @@ public class PlayerMovement : MonoBehaviour
     private void MoveTo(Vector2 movement)
     {
         transform.Translate(movement);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            rb.linearVelocity = Vector2.zero;
+            EditorApplication.Beep();
+        }
     }
 }
